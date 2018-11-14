ec3452fb78dafa090e91ce7f924b6e6446d8ff63
Peter Dahlén <36642622+peterdahlen@users.noreply.github.com>
2018-11-14 16:06:48 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -9,28 +9,67 @@ import DeskPad from "./components/DeskPad";
 import LongPad from './components/LongPad';
 import FooterShipping from './components/FooterShipping';
 import Footer from "./components/Footer";
+import { connect } from 'react-redux';
+import Cart from './components/shopify/Cart';
+import store from './store';
 
 class App extends Component {
 
+  constructor() {
+    super();
+    this.updateQuantityInCart = this.updateQuantityInCart.bind(this);
+    this.removeLineItemInCart = this.removeLineItemInCart.bind(this);
+    this.handleCartClose = this.handleCartClose.bind(this);
+    this.handleCartOpen = this.handleCartOpen.bind(this);
+  }
+  updateQuantityInCart(lineItemId, quantity) {
+    const state = store.getState(); // state from redux store
+    const checkoutId = state.checkout.id
+    const lineItemsToUpdate = [{id: lineItemId, quantity: parseInt(quantity, 10)}]
+    state.client.checkout.updateLineItems(checkoutId, lineItemsToUpdate).then(res => {
+      store.dispatch({type: 'UPDATE_QUANTITY_IN_CART', payload: {checkout: res}});
+    });
+  }
+  removeLineItemInCart(lineItemId) {
+    const state = store.getState(); // state from redux store
+    const checkoutId = state.checkout.id
+    state.client.checkout.removeLineItems(checkoutId, [lineItemId]).then(res => {
+      store.dispatch({type: 'REMOVE_LINE_ITEM_IN_CART', payload: {checkout: res}});
+    });
+  }
+  handleCartClose() {
+    store.dispatch({type: 'CLOSE_CART'});
+  }
+  handleCartOpen() {
+    store.dispatch({type: 'OPEN_CART'});
+  }
+ 
   render() {
-
+    const state = store.getState(); // state from redux store 
     return (
       <BrowserRouter>
-      <ScrollToTop>
-        <div>
-          <Navbar />
-          <Route path="/" component={Home} exact  />
-          <Route path="/castor-:name" component={Castor} />
-          <Route path="/avior-:name" component={Avior} />
-          <Route path="/desk-pad-:name" component={DeskPad} />
-          <Route path="/long-pad-:name" component={LongPad} />
-          <Footer />
-          <FooterShipping />
-        </div>
+        <ScrollToTop>
+          <div>
+            <Navbar handleCartOpen={this.handleCartOpen} />
+            <Route path="/" render={() => (<Home cookies={this.props.cookies}/>)} exact  />
+            <Route path="/castor-:name" component={Castor} />
+            <Route path="/avior-:name" component={Avior} />
+            <Route path="/desk-pad-:name" component={DeskPad} />
+            <Route path="/long-pad-:name" component={LongPad} />
+            <Cart
+              checkout={state.checkout}
+              isCartOpen={state.isCartOpen}
+              handleCartClose={this.handleCartClose}
+              updateQuantityInCart={this.updateQuantityInCart}
+              removeLineItemInCart={this.removeLineItemInCart} />
+            <Footer />
+            <FooterShipping />
+          </div>
         </ScrollToTop>
+        
       </BrowserRouter>
     );
   }
 }
 
-export default App;
+export default connect((state) => state)(App);
